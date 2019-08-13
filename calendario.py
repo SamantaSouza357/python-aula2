ca62b0411b06809b1803e13278131d8caa249fdf
samantasouza357 <samantasouza931@gmail.com>
2019-08-13 17:11:24 -0300

    funcionarios e calendario

---
+++ calendario.py
@@ -0,0 +1,7 @@
+dia_da_semana = {1:'domingo', 2:'segunda-feira',3:'terça-feira',4:'quarta-feira'
+,5:'quinta-feira',6:'sexta-feira',7:'sabado'}
+
+dia = int(input('digite o dia da semana: '))
+ 
+print(dia_da_semana[dia])
+                 
---
+++ funcionarios.py
@@ -0,0 +1,9 @@
+nome = input('digite seu nome: ')
+horas_trabalhadas = float(input('digite suas horas trabalhadas: '))
+valor_horas_trabalhadas = float(input('valor por horas trabalhadas: '))
+
+salario = horas_trabalhadas * valor_horas_trabalhadas
+
+
+print(nome + ' seu salario é : ' + str (salario) + ' reais ')
+
